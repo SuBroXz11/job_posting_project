eded7cced3550de95d73259c0932108d0b2c1c16
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-29 14:42:49 +0545

    tailwind css added and eslint removed

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,9 @@
 const HomeScreen = () => {
     return (
-        <div>
-            Home
+        <div className="h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">
+                Hello, Tailwind CSS!
+            </h1>
         </div>
     )
 }
